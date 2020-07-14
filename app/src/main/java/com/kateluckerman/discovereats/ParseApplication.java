4515c7abd4f65e460dd9777cc86b777095167486
kateluckerman <kateluckerman@gmail.com>
2020-07-14 11:03:26 -0500

    Added Parse client to project

---
+++ app/src/main/java/com/kateluckerman/discovereats/ParseApplication.java
@@ -0,0 +1,34 @@
+package com.kateluckerman.discovereats;
+
+import android.app.Application;
+import com.parse.Parse;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+// reference : https://guides.codepath.org/android/Building-Data-driven-Apps-with-Parse#setup
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Use for troubleshooting -- remove this line for production
+        Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
+
+        // Use for monitoring Parse OkHttp traffic
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+
+        // set applicationId, and server server based on the values in the Heroku settings.
+        // clientKey is not needed unless explicitly configured
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("discover-eats") // should correspond to APP_ID env variable
+                .clientKey("keytoeverything")  // set explicitly unless clientKey is explicitly configured on Parse server
+                .server("https://discover-eats.herokuapp.com/parse/").build());
+    }
+}
+
